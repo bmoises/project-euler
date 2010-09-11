e0eb864b68155ac543f7acde2d0ad61a720eafc3
Moises Beltran <BMoises@gmail.com>
2010-09-10 22:11:04 -0700

    Decided to add problem to .rb file

--- problems/1-100/1/problem.rb
+++ problems/1-100/1/problem.rb
@@ -1,14 +1,16 @@
 # natural numbers: 1, 2, 3, ..... , n
+#If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+#Find the sum of all the multiples of 3 or 5 below 1000.
 
 sum = 0
 n = 0
 upto = ARGV.first || 1000
-while n <= upto
+while n < upto
   if n % 3 == 0 || n % 5 == 5
     sum += n
   end
   n+=1
 end
 
-puts "Sum of all natural numbers multiples of 3 or 5"
+puts "Sum of all natural numbers multiples of 3 or 5 below #{upto}"
 puts sum
